969ba000debd4398efc533c8f0bc1da5f682bc75
lalitchoudhry <choudharylalit182@gmail.com>
2026-07-24 20:19:26 +0530

    Add import for about page CSS styles in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/portfolio-tokens.css'
 import './styles/portfolio-page.css'
+import './styles/about-page.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
